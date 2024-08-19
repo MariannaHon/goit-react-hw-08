ade2c1a62608e98db2f65a06e095744caa597f90
MariannaHon <imarianna96i@gmail.com>
2024-08-19 16:28:19 +0200

    commit

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from 'react-hot-toast';
 
-axios.defaults.baseURL = "https://connections-api.herokuapp.com/";
+axios.defaults.baseURL = "https://phonebook-db-d0ot.onrender.com/";
 
 export const fetchContacts = createAsyncThunk("contacts/fetchAll", async (_, thunkAPI) => {
     try {
